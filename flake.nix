1aa1778517bd521ce9b68c79ddd3b8383f04b31e
Ole Krüger <ole@vprsm.de>
2022-07-18 19:56:36 +0100

    Include both hard drives in Grub config

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
         boot.loader.grub.enable = true;
         boot.loader.grub.version = 2;
-        boot.loader.grub.device = "/dev/sda";
+        boot.loader.grub.devices = [ "/dev/sda" "/dev/sdb" ];
 
         fileSystems."/" = {
           device = "/dev/disk/by-label/nixos";
