798638d336618da430759e52fb8835bcbb9be5b7
Ole Krüger <ole@vprsm.de>
2022-07-18 22:10:12 +0100

    Only enable Nix Store serving via SSH on ded1

--- flake.nix
+++ flake.nix
@@ -70,20 +70,6 @@
 
           registry.nixpkgs.flake = nixpkgs;
 
-          sshServe = {
-            enable = true;
-            write = true;
-
-            keys = [
-              # Nix Cache Use (hrel)
-              "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDCaUKnuSXxgS50BdWZgrtsmeJ/s5vD65lQTagAp8OUrc4uI4S+H7aGMP79TQdKwm9unNxGWS0WudceOyYzUyno="
-              # Desktop
-              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKKRTgvRNbWuT85F9/qKZy9cJ47bC7kZhEgDz/dC24F+WQC7sV+MrimPb7cuyRyL7YAgf5kebM97eUa1rMNYfX+avD17pCW/3bVu37VPN+s3UHECSFN0BNTT0D0sIWXHcJdZkoa2UUEMCPzzjBpOQLNtoUQhdkrwJIS7KMjw9DJWf6gCGzUEYW2mzhnIzL5OeBHD4IGLKaIzAANKxALDIugfUhB3WlMUidhlu1TyaXvqsJ7Vj2OfX2Dv75wZIjeBRZse4Yq9fMWjiUcZds88eBX6y7ts7zzv0jd1pbThc7PIQLOsHAOapOFElS6gZlLOf+9k9f9j3LPFi8xu07OZCP"
-            ];
-          };
-
-          settings.trusted-users = [ "nix-ssh" ];
-
           gc.automatic = true;
         };
       };
@@ -98,6 +84,24 @@
 
         system.configurationRevision = lib.mkIf (self ? rev) self.rev;
       };
+
+      serve-nix-store = { config, ... }: {
+        nix = {
+          sshServe = {
+            enable = true;
+            write = true;
+
+            keys = [
+              # Nix Cache Use (hrel)
+              "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDCaUKnuSXxgS50BdWZgrtsmeJ/s5vD65lQTagAp8OUrc4uI4S+H7aGMP79TQdKwm9unNxGWS0WudceOyYzUyno="
+              # Desktop
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKKRTgvRNbWuT85F9/qKZy9cJ47bC7kZhEgDz/dC24F+WQC7sV+MrimPb7cuyRyL7YAgf5kebM97eUa1rMNYfX+avD17pCW/3bVu37VPN+s3UHECSFN0BNTT0D0sIWXHcJdZkoa2UUEMCPzzjBpOQLNtoUQhdkrwJIS7KMjw9DJWf6gCGzUEYW2mzhnIzL5OeBHD4IGLKaIzAANKxALDIugfUhB3WlMUidhlu1TyaXvqsJ7Vj2OfX2Dv75wZIjeBRZse4Yq9fMWjiUcZds88eBX6y7ts7zzv0jd1pbThc7PIQLOsHAOapOFElS6gZlLOf+9k9f9j3LPFi8xu07OZCP"
+            ];
+          };
+
+          settings.trusted-users = [ "nix-ssh" ];
+        };
+      };
     };
 
     nixosConfigurations = {
@@ -106,6 +110,7 @@
 
         modules = with self.nixosModules; [
           common
+          serve-nix-store
           ({ config, ... }: {
             networking.interfaces.enp4s0.ipv6.addresses = [
               {
