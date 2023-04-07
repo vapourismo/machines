d8248d383fa5a479c8eb4494363f93ac088ca914
Ole Krüger <ole@vprsm.de>
2023-04-07 14:09:58 +0100

    Enable Hydra

---
+++ components/hydra.nix
@@ -0,0 +1,26 @@
+{config, ...}: {
+  services.hydra = {
+    enable = true;
+    listenHost = "localhost";
+    hydraURL = "https://hydra.hwlium.com";
+    notificationSender = "hydra@hwlium.com";
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts."hydra.hwlium.com" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${builtins.toString config.services.hydra.port}/";
+        extraConfig = ''
+          proxy_ssl_server_name on;
+        '';
+      };
+    };
+  };
+}
