10ee4b5b626036046df752f1e8caeb486f8d5bca
Ole Krüger <ole@vprsm.de>
2023-04-14 20:10:44 +0100

    Add more build machines

--- common/nix.nix
+++ common/nix.nix
@@ -11,5 +11,33 @@
     registry.nixpkgs.flake = specialArgs.inputs.nixpkgs;
 
     gc.automatic = true;
+    distributedBuilds = true;
+
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+      {
+        hostName = "eu.nixbuild.net";
+        system = "i686-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+      {
+        hostName = "eu.nixbuild.net";
+        system = "armv7l-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+      {
+        hostName = "eu.nixbuild.net";
+        system = "aarch64-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+    ];
   };
 }
