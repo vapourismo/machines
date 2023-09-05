e7eceb5614eba579c172e1acda9b9c1372da09fa
Ole Krüger <git@ole.lol>
2023-09-05 20:41:51 +0100

    Enable more nginx jails for fail2ban

--- common/fail2ban.nix
+++ common/fail2ban.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{...}: {
   services.fail2ban = {
     enable = true;
     maxretry = 5;
@@ -8,6 +8,31 @@
       "172.16.0.0/12"
       "192.168.0.0/16"
     ];
+    jails = {
+      nginx-http-auth = ''
+        enabled = true
+        port    = http,https
+        logpath = %(nginx_error_log)s
+      '';
+
+      nginx-limit-req = ''
+        enabled = true
+        port    = http,https
+        logpath = %(nginx_error_log)s
+      '';
+
+      nginx-botsearch = ''
+        enabled  = true
+        port     = http,https
+        logpath  = %(nginx_error_log)s
+      '';
+
+      nginx-bad-request = ''
+        enabled = true
+        port    = http,https
+        logpath = %(nginx_access_log)s
+      '';
+    };
   };
 
   services.openssh.settings.LogLevel = "VERBOSE";
