842490068b5ebe22876e61b7de6d62241e279823
Ole Krüger <git@ole.lol>
2024-03-12 14:53:27 +0000

    Allow work laptop access

--- common/ssh.nix
+++ common/ssh.nix
@@ -13,6 +13,8 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB6nFifbv41ZC+L5fJcaiihHb3pzFkX4FKhpjeuCuHxn root@fi1"
     # NuttenBook Pro
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7vlc902QXTseSF7NsFy3CouUnWFQWDFy1EvS0CRD5q ole@ole-darwin.home"
+    # Work Laptop
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBES0D7Kdt/0JRFmgbpWq18xxCMO28bTCEzwMDB6GIyV1LHGhLuZceWl826fOM3wsta61FzLUGaaNpBOItEJ4Lhc= ole@Oles-MBP.net"
   ];
 
   programs.ssh = {
