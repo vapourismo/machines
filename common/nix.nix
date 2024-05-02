45b0b4403bebdaaa6744d100cc7caea615cb0566
Ole Krüger <git@ole.lol>
2024-05-02 19:30:02 +0000

    Disable automatic garbage collection

--- common/nix.nix
+++ common/nix.nix
@@ -2,11 +2,12 @@
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
+      min-free = ${toString (1024 * 1024 * 1024)}
+      max-free = ${toString (10 * 1024 * 1024 * 1024)}
     '';
 
     registry.nixpkgs.flake = specialArgs.inputs.nixpkgs;
 
     optimise.automatic = true;
-    gc.automatic = true;
   };
 }
